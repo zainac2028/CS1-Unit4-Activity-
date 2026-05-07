fe12bec22b3c75293275926cedffb18406f16171
zainac2028 <zaina.cisse2028@gbwl.org>
2026-05-07 15:55:08 +0000

    toggle mode function

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ let darkMode = false
  // JS can now change attributes and properties!
  heading.textContent = "Light and Dark Website"; // JS wrote HTML code!
  heading.style.color = "crimson"; // JS wrote CSS code!
+
+ // Define what happens when button is clicked 
+function toggleMode() {
+   console.log("triggered function")
+   // 1. Flip is the boolean to reflect the mode
+   darkMode = !darkMode;
+   console.log(darkMode);
+   // 2. Conditionally apply styles based on mode
+   if (darkMode == true) {    
+      console.log ("apply dark styles");
+      body.style.background = "darkred";
+      heading.style.color = "orange";
+      description.style.color = "white";
+      description.textContent = "Rellow Side";
+      toggleBtn.style.color = "white"
+      toggleBtn.textContent = "Switch to Yed";
+   }
+   else {
+      console.log ("apply light styles");
+      body.style.background = "orange";
+      heading.style.color = "darkred";
+      description.style.color = "black";
+      description.textContent = "Yed Side";
+      toggleBtn.textContent = "Switch to Rellow";
+   }
+
+}
+// Attach function to the actual button
+toggleBtn.addEventListener("click", toggleMode);
