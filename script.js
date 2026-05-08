1de37bb7c4dc0e3bb8ad13f8e9292500999acba4
zainac2028 <zaina.cisse2028@gbwl.org>
2026-05-08 16:40:09 +0000

    added images changed the colors

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let darkMode = false
  const heading = document.querySelector("#main-heading");
  const description = document.querySelector("#description");
  const toggleBtn = document.querySelector("#toggle")
+ const image = document.querySelector ("#image")
  // CONST is like LET, but the value won't change
 
  // JS can now change attributes and properties!
@@ -25,20 +26,21 @@ function toggleMode() {
    // 2. Conditionally apply styles based on mode
    if (darkMode == true) {    
       console.log ("apply dark styles");
-      body.style.background = "darkred";
-      heading.style.color = "orange";
+      body.style.background = "crimson";
+      heading.style.color = "white";
       description.style.color = "white";
-      description.textContent = "Rellow Side";
-      toggleBtn.style.color = "white"
-      toggleBtn.textContent = "Switch to Yed";
+      description.textContent = "Strawberry Jam ";
+      toggleBtn.textContent = "Rabbit Clown";
+      image.src = "redsonny.webp";
    }
    else {
       console.log ("apply light styles");
-      body.style.background = "orange";
-      heading.style.color = "darkred";
-      description.style.color = "black";
-      description.textContent = "Yed Side";
-      toggleBtn.textContent = "Switch to Rellow";
+      body.style.background = "black";
+      heading.style.color = "white";
+      description.style.color = "white";
+      description.textContent = "Rabbit Clown";
+      toggleBtn.textContent = "Strawberry Jam";
+      image.src = "blacksonny.png";
    }
 
 }
